dcaa58c642fda4e465af54b4e8cc50bfccdae95e
andresistuk <andresistuk@knu.ua>
2017-10-23 00:43:06 +0300

    Add folder + Chapter 1 1.2 - Labs (1) + Звіт

---
+++ Chapter 1/1.2 - Labs (1)/1.2 - Labs (1).cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "It's me, your first program. ";
+	cout << "Hello, Andriy \n";
+	cout << "Hello, Mycola";
+	cout << endl;
+	cout << "Hello, Nadia ";
+	return 0;
+}
