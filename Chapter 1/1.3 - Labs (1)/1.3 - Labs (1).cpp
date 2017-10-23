c3a58e1008c785eef21eb33fccbb5814c4921240
andresistuk <andresistuk@knu.ua>
2017-10-23 10:02:19 +0300

    Add folder + Chapter 1 1.3 - Labs (1) + Звіт

---
+++ Chapter 1/1.3 - Labs (1)/1.3 - Labs (1).cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+int main()
+{
+int result=10800; // 3*60*60
+int ThreeMinutesInSeconds=3*60;
+int SixMinutesInSeconds=6*60;
+float pi=3.141526; //This is the value of pi
+std::cout << "result: " << result << std::endl;
+}
