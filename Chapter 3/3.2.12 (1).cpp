92935d4a598d47c4961467ec2fcf977e8bbb13e5
andresistuk <andresistuk@knu.ua>
2017-11-17 02:55:26 +0200

    folder + Chapter 3 Lab 3.2

---
+++ Chapter 3/3.2.12 (1).cpp
@@ -0,0 +1,23 @@
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+
+	int vector[] = { 3, -5, 7, 10, -4, 14, 5, 2, -13 };
+	int n = sizeof(vector) / sizeof(vector[0]);
+	int *currentElement = vector;
+	int minimum = 0;
+
+	for (int i = 0; i < n; i++) {
+		if (minimum > *currentElement) {
+			minimum = *currentElement;
+		}
+		currentElement++;
+	}
+
+	cout << "Minimum element is " << minimum << endl;
+
+	return 0;
+}
---
+++ Chapter 3/3.2.12 (2).cpp
@@ -0,0 +1,27 @@
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+
+	int matrix[10][10] = { };
+	int *currentElement = matrix[0];
+
+	for (int i = 1; i <= 10; i++) {
+		for (int j = 1; j <= 10; j++) {
+			*currentElement = j * i;
+			currentElement++;
+		}
+	}
+
+	for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < 10; j++) {
+			cout.width(4);
+			cout << matrix[i][j];
+		}
+		cout << endl;
+	}
+
+	return 0;
+}
